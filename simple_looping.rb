88d0e6025a1077e140251b6e68f1add4fdf0b070
David Gilbert <dagilbert333@gmail.com>
2018-09-23 00:50:27 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,7 +35,8 @@ def until_iterator(number_of_times)
   counter = 0 
   until counter == number_of_times
     puts phrase = "Welcome to Flatiron School's Web Development Course!"
-    
+    counter += 1 
+  end
 end
 
 def for_iterator(number_of_times)
